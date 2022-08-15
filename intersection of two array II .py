eb820a5c8e7d544ceb343e63ee46f45a7989b8e4
Vivek Raj <81849302+vivek625@users.noreply.github.com>
2022-08-15 21:47:18 +0530

    Create intersection of two array II .py

---
+++ intersection of two array II .py
@@ -0,0 +1,18 @@
+# Vivek raj singh
+class Solution:
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        nums1.sort()
+        nums2.sort()
+        i = 0
+        j = 0
+        s = []
+        while (i<len(nums1) and (j<len(nums2))):
+            if nums1[i] == nums2[j]:
+                s.append(nums1[i])
+                i+=1
+                j+=1
+            elif nums1[i]<nums2[j]:
+                i+=1
+            else:
+                j+=1
+        return s
